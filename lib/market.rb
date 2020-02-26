69dc87dfe13fe78640994a239e2e738a72838584
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-02-26 11:59:49 -0700

    Complete iteration 3

--- lib/market.rb
+++ lib/market.rb
@@ -17,4 +17,57 @@ class Market
   def vendors_that_sell(item)
     @vendors.find_all {|vendor| vendor.inventory.include?(item)}
   end
+
+  def find_total_items
+    total_items = []
+    @vendors.each do |vendor|
+      total_items << vendor.inventory.keys
+    end
+    total_items = total_items.flatten.uniq
+  end
+
+  def items_and_quantity
+    total_items = []
+    @vendors.each do |vendor|
+      total_items << vendor.inventory
+    end
+    total_items = total_items.flatten.uniq
+
+    my_new_hash = {}
+    total_items.each do |items|
+      items.each do |item|
+        if my_new_hash.has_key?(item.first)
+          my_new_hash[item.first] += item.last
+        else
+          my_new_hash[item.first] = item.last
+        end
+      end
+    end
+    my_new_hash
+  end
+
+  def find_item_quantity(item)
+    x = items_and_quantity
+    x[item]
+  end
+
+  def total_inventory
+    market_items = {}
+    find_total_items.each do |item|
+      market_items[item] = {quantity: find_item_quantity(item),
+                            vendors: vendors_that_sell(item)}
+    end
+    market_items
+  end
+
+  def overstocked_items
+    found = total_inventory.find_all do |items|
+      items[1].values.first > 50 && items[1].values.last.length > 1
+    end.flatten
+    [found.first]
+  end
+
+  def sorted_item_list
+    find_total_items.map {|item| item.name}.sort
+  end
 end
--- test/market_test.rb
+++ test/market_test.rb
@@ -65,6 +65,69 @@ class MarketTest < Minitest::Test
     assert_equal [@vendor1, @vendor3], @market.vendors_that_sell(@item1)
     assert_equal [@vendor2], @market.vendors_that_sell(@item4)
   end
+
+  def test_it_can_return_total_inventory
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+    @vendor3.stock(@item3, 10)
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+    expected = {
+                @item1 => {:quantity => 100, :vendors => [@vendor1, @vendor3]},
+                @item2 => {:quantity => 7, :vendors => [@vendor1]},
+                @item4 => {:quantity => 50, :vendors => [@vendor2]},
+                @item3 => {:quantity => 35, :vendors => [@vendor2, @vendor3]}
+                }
+
+    assert_equal expected, @market.total_inventory
+  end
+
+  def test_it_can_find_total_items
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+    @vendor3.stock(@item3, 10)
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    assert_equal [@item1, @item2, @item4, @item3], @market.find_total_items
+  end
+
+  def test_it_can_return_overstocked_items
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+    @vendor3.stock(@item3, 10)
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    assert_equal [@item1], @market.overstocked_items
+  end
+
+  def test_it_can_sort_list
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+    @vendor3.stock(@item3, 10)
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+    expected = ["Banana Nice Cream", "Peach", "Peach-Raspberry Nice Cream", "Tomato"]
+
+    assert_equal expected, @market.sorted_item_list
+  end
 end
 
 
@@ -75,79 +138,7 @@ end
 #
 # You Market will also be able to identify overstocked_items. An item is overstocked if it is sold by more than 1 vendor AND the total quantity is greater than 50.
 #
-# Use TDD to update your Market class so that it responds to the following interaction pattern:
-#
-# pry(main)> require './lib/item'
-# #=> true
-#
-# pry(main)> require './lib/vendor'
-# #=> true
-#
-# pry(main)> require './lib/market'
-# #=> true
-#
-# pry(main)> market = Market.new("South Pearl Street Farmers Market")
-# #=> #<Market:0x00007fe134933e20...>
-#
-# pry(main)> item1 = Item.new({name: 'Peach', price: "$0.75"})
-# #=> #<Item:0x007f9c56740d48...>
-#
-# pry(main)> item2 = Item.new({name: 'Tomato', price: '$0.50'})
-# #=> #<Item:0x007f9c565c0ce8...>
-#
-# pry(main)> item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
-# #=> #<Item:0x007f9c562a5f18...>
-#
-# pry(main)> item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
-# #=> #<Item:0x007f9c56343038...>
-#
-# pry(main)> vendor1 = Vendor.new("Rocky Mountain Fresh")
-# #=> #<Vendor:0x00007fe1348a1160...>
-#
-# pry(main)> vendor1.stock(item1, 35)
-#
-# pry(main)> vendor1.stock(item2, 7)
-#
-# pry(main)> vendor2 = Vendor.new("Ba-Nom-a-Nom")
-# #=> #<Vendor:0x00007fe1349bed40...>
-#
-# pry(main)> vendor2.stock(item4, 50)
-#
-# pry(main)> vendor2.stock(item3, 25)
-#
-# pry(main)> vendor3 = Vendor.new("Palisade Peach Shack")
-# #=> #<Vendor:0x00007fe134910650...>
-#
-# pry(main)> vendor3.stock(item1, 65)
-#
-# pry(main)> vendor3.stock(item3, 10)
-#
-# pry(main)> market.add_vendor(vendor1)
-#
-# pry(main)> market.add_vendor(vendor2)
-#
-# pry(main)> market.add_vendor(vendor3)
-#
-# pry(main)> market.total_inventory
-# #=> {
-#   #   #<Item:0x007f9c56740d48...> => {
-#   #     quantity: 100,
-#   #     vendors: [#<Vendor:0x00007fe1348a1160...>, #<Vendor:0x00007fe134910650...>]
-#   #   },
-#   #   #<Item:0x007f9c565c0ce8...> => {
-#   #     quantity: 7,
-#   #     vendors: [#<Vendor:0x00007fe1348a1160...>]
-#   #   },
-#   #   #<Item:0x007f9c56343038...> => {
-#   #     quantity: 50,
-#   #     vendors: [#<Vendor:0x00007fe1349bed40...>]
-#   #   },
-#   #   #<Item:0x007f9c562a5f18...> => {
-#   #     quantity: 35,
-#   #     vendors: [#<Vendor:0x00007fe1349bed40...>, #<Vendor:0x00007fe134910650...>]
-#   #   },
-#   # }
-#
+
 # pry(main).overstocked_items
 # #=> [#<Item:0x007f9c56740d48...>]
 #
